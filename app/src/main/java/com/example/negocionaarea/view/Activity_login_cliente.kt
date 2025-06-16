94679c698f718f5a603c069b13bee1f768027b13
Taynad <166086044+Taynad@users.noreply.github.com>
2025-06-16 19:31:33 -0300

    fix: correção tela cliente

--- app/src/main/java/com/example/negocionaarea/view/Activity_login_cliente.kt
+++ app/src/main/java/com/example/negocionaarea/view/Activity_login_cliente.kt
@@ -12,7 +12,7 @@ class Activity_login_cliente : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login_cliente)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.buttonCadastrarEmpresa)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
