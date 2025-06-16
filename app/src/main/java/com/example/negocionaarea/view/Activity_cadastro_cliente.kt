9c1bcb27d1aec8b50fa8cebb5206f9acb357c6ce
Taynad <166086044+Taynad@users.noreply.github.com>
2025-06-16 18:44:05 -0300

    style: criação tela de login do cliente

--- app/src/main/java/com/example/negocionaarea/view/Activity_cadastro_cliente.kt
+++ app/src/main/java/com/example/negocionaarea/view/Activity_cadastro_cliente.kt
@@ -1,10 +1,11 @@
-package com.example.negocionaarea
+package com.example.negocionaarea.view
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.negocionaarea.R
 
 class activity_cadastro_cliente : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
