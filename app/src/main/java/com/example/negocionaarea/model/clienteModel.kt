fcf42ef75e268e2aa7cbeafcc9b2f3d1f3f5ed92
Taynad <166086044+Taynad@users.noreply.github.com>
2025-06-16 21:00:31 -0300

    feat: criar classe model cliente

---
+++ app/src/main/java/com/example/negocionaarea/model/clienteModel.kt
@@ -0,0 +1,9 @@
+package com.example.negocionaarea.model
+
+data class clienteModel (
+        var nomeCliente: String = "",
+        var emailCliente: String = "",
+        var telefoneCliente: String = "",
+        var senhaCliente: String = ""
+
+)
